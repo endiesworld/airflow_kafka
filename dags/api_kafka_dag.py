4e8af9d23711f7d88ae2cd348763b356b4110459
endiesworld <endiesworld@gmail.com>
2024-05-15 03:07:31 +0100

    feat: Setup kafka connections

--- dags/api_kafka_dag.py
+++ dags/api_kafka_dag.py
@@ -1,15 +1,11 @@
 import uuid
-import json
 from datetime import datetime
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.http.sensors.http import HttpSensor
-from airflow.providers.http.operators.http import SimpleHttpOperator
 
 default_args = {
-    'owner': 'Emmanuel',
-    'start_date': datetime(2024, 5, 10, 3, 45)
+    'owner': 'emmanuel',
+    'start_date': datetime(2024, 5, 15, 10, 00)
 }
 
 def get_data():
@@ -24,7 +20,7 @@ def get_data():
 def format_data(res):
     data = {}
     location = res['location']
-    data['id'] = uuid.uuid4()
+    # data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -41,46 +37,38 @@ def format_data(res):
     return data
 
 def stream_data():
+    import json
     from kafka import KafkaProducer
     import time
     import logging
-
-    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    curr_time = time.time()
-
-    while True:
-        if time.time() > curr_time + 60: #1 minute
-            break
-        try:
-            res = get_data()
-            res = format_data(res)
-
-            producer.send('users_created', json.dumps(res).encode('utf-8'))
-        except Exception as e:
-            logging.error(f'An error occured: {e}')
-            continue
-
-with DAG('user_processing',
-         default_args=default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
     
-    is_api_available = HttpSensor(
-        task_id='is_api_available',
-        http_conn_id='user_api',
-        endpoint='api/'
-    )
+    res = get_data()
+    res = format_data(res)
 
-    # extract_user = SimpleHttpOperator(
-    #     task_id='extract_user',
-    #     http_conn_id='user_api',
-    #     endpoint='api/',
-    #     method='GET',
-    #     response_filter=lambda response: json.loads(response.text),
-    #     log_response=True
-    # )
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    curr_time = time.time()
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
-    streaming_task = PythonOperator(
-        task_id='fetach_data_from_api',
-        python_callable=stream_data
-    )
+    # while True:
+    #     if time.time() > curr_time + 60: #1 minute
+    #         break
+    #     try:
+    #         res = get_data()
+    #         res = format_data(res)
+
+    #         producer.send('users_created', json.dumps(res).encode('utf-8'))
+    #     except Exception as e:
+    #         logging.error(f'An error occured: {e}')
+    #         continue
+
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
+
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+#         python_callable=stream_data
+#     )
+
+stream_data()
